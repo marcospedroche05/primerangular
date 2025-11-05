94aa5c93b57d417e10f98b61dff5d701b72fc7d5
marcospedroche05 <marcospedroche62@gmail.com>
2025-11-05 09:45:37 +0100

    directivas style

--- src/components/deportes/deportes.component.ts
+++ src/components/deportes/deportes.component.ts
@@ -9,9 +9,11 @@ import { Component } from "@angular/core";
 export class DeportesComponent {
 
     public deportes: Array<string>;
+    public numeros: Array<number>;
 
     constructor(){
         this.deportes = ["Futbol", "Baloncesto", "Dardos", "Petanca"];
+        this.numeros = [4,5,6,7,8,9];
     }
 
 
